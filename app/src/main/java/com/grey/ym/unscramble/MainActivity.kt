b982050cc033c9236aba886470bcb51162a8314f
Grey <yakubov.grey@gmail.com>
2026-03-13 12:11:35 +0400

    Initial commit

---
+++ app/src/main/java/com/grey/ym/unscramble/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.grey.ym.unscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.grey.ym.unscramble.ui.GameScreen
+import com.grey.ym.unscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnscrambleTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/grey/ym/unscramble/ui/GameUiState.kt
@@ -0,0 +1,9 @@
+package com.grey.ym.unscramble.ui
+
+data class GameUiState(
+    val currentScrambledWord: String = "",
+    val currentWordCount: Int = 1,
+    val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false,
+    val score: Int = 0
+)
